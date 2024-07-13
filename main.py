bca7fb1495182d754077e5fd5f350e05c82248fc
Nicola-EL <eslis2004@gmail.com>
2024-07-13 19:19:48 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,45 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+index = [0]
+index_1 = [1]
+index_2 = [2]
+index_3 = [3]
+index_4 = [4]
+index_5 = [5]
+index_6 = [6]
+index_7 = [7]
+index_8 = [8]
+index_9 = [9]
+index_10 = [10]
+index_11 = [11]
+while len(index)>0:
+    print(42)
+    if len(index_1)>0:
+        print(69)
+        if len(index_2)>0:
+            print(322)
+            if len(index_3) > 0:
+                print(13)
+                if len(index_5) > 0:
+                    print(99)
+                    if len(index_7) > 0:
+                        print(9)
+                        if len(index_8) > 0:
+                            print(8)
+                            if len(index_9) > 0:
+                                print(7)
+                                if len(index_11) > 0:
+                                    print(5)
+                                    break
+                                else:
+                                    print(0)
+                                    print(-5)
+                                    print(-6)
+
+
+
+
+
+
+
+
+
